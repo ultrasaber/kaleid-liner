9bab497770a03e052c4926a34575db51eac67b25
Kalizorah <saberthesaber@gmail.com>
2016-06-09 17:45:47 -0500

    Additional command-line flags
    --file, --css, --preClass, --codeClass

--- kaleid-liner.js
+++ kaleid-liner.js
@@ -6,23 +6,41 @@
 
 // Imports
 var fs = require("fs");
+var flags = require("flags");
+
+// Define command-line flags
+flags.defineString("file").setDescription("The input filename.");
+flags.defineBoolean("css").setDescription("When TRUE, includes a CSS snippet for demonstration.");
+flags.defineString("preClass").setDescription("The CSS class to be used for <pre> elements.");
+flags.defineString("codeClass").setDescription("The CSS class to be used for <code> elements.");
+
+flags.parse();
 
 // CSS markup for line numbering
 var cssMarkup = "<style>\npre\n{\n  counter-reset: line;\n}\n\ncode\n{\n  counter-increment: line;\n}\n\ncode:before\n{\n  display: inline-block;\n  content: counter(line);\n  -webkit-user-select: none;\n  width: 30px;\n}\n</style>\n"
 
+// Create CSS classes.
+function createCSSClass(name)
+{
+	return name === undefined ? "" : " class=\""+name+"\"";
+}
+
+var preClass = createCSSClass(flags.get("preClass"));
+var codeClass = createCSSClass(flags.get("codeClass"));
+
 // Get input filename from command-line args
-var inputFileName = process.argv[2];
+var inputFileName = flags.get("file");
 
 // Read data from the file
 var inputData = fs.readFileSync(inputFileName).toString();
 var lines = inputData.split("\n");
 
 // Generate output data
-var outputData = "<pre>\n"; // Open tags
+var outputData = "<pre" + preClass + ">\n"; // Open tags
 
 lines.forEach(function(line)
 		{
-			var outputLine = "<code>" + line.replace(/(\r\n|\n|\r)/gm,"").replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') + "</code>\n"; // Escape HTML tags.
+			var outputLine = "<code" + codeClass + ">" + line.replace(/(\r\n|\n|\r)/gm,"").replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') + "</code>\n"; // Escape HTML tags.
 			outputData += outputLine;
 		});
 
@@ -30,8 +48,13 @@ outputData += "</pre>"; // close tags
 
 console.log(outputData);
 
+console.log("preclass: " + preClass);
+console.log("codeclass: " + codeClass);
+
 // Write output data to file
-fs.writeFile(inputFileName + ".html", cssMarkup + "\n" +  outputData, function(err)
+fs.writeFile(inputFileName + ".html", 
+		(flags.get("css") ? cssMarkup + "\n" : "") + outputData, 
+		function(err)
 		{
 			if(err){
 				return console.error(err);
